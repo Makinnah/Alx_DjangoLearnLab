308a2a60bd4ee2c2ecf2231cedb92be3438543f1
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-09-07 19:10:03 +0300

    Add admin_view.html template for Admin role view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -60,6 +60,7 @@ def is_member(user):
 
 @user_passes_test(is_admin)
 def admin_view(request):
+"""View accessible only to users with the Admin role."""
     return render(request, 'relationship_app/admin_view.html')
 
 @user_passes_test(is_librarian)
