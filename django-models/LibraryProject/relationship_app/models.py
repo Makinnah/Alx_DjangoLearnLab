fd9edb9e0c9836e3adf84f60c0cb4361d90a9e6c
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-09-07 18:18:22 +0300

    Fix: remove embedded repo and add project files properly

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    publication_year = models.IntegerField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    library = models.ForeignKey(Library, related_name='books', on_delete=models.CASCADE)
