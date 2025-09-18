7e546b005706a4c5234ab69f0d1c1495995a672d
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-09-18 14:51:06 +0300

    Add BookViewSet with CRUD operations using DRF routers

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,4 +1,5 @@
 # api/views.py
+from rest_framework import viewsets
 from rest_framework import generics
 from .models import Book
 from .serializers import BookSerializer
@@ -7,3 +8,12 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+
+
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    A viewset that provides the standard CRUD operations
+    for the Book model (list, create, retrieve, update, delete).
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
