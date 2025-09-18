695122993f030e3c1ecdcd94e3e613109d5c8f2b
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-09-18 15:15:13 +0300

    Added authentication and permissions to API

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -17,3 +17,14 @@ class BookViewSet(viewsets.ModelViewSet):
     """
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+#Permission Classes to Views
+from rest_framework import viewsets, permissions
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
