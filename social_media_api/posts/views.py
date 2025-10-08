987660d6f2ee3a39ba02512a693ac3c86f4d97e8
Duncan Osuka <duncan.osuka@ea.aku.edu>
2025-10-08 16:02:52 +0300

    Added FeedView to show posts from followed users ordered by creation date

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -51,27 +51,15 @@ class CommentViewSet(viewsets.ModelViewSet):
 #Task 2
 # posts/views.py
 from rest_framework import generics, permissions
-from rest_framework.pagination import PageNumberPagination
-from django.contrib.auth import get_user_model
-from django.shortcuts import get_object_or_404
-from django.db.models import Q
-
 from .models import Post
-from .serializers import PostSerializer  # assume exists
-
-User = get_user_model()
-
-class FeedPagination(PageNumberPagination):
-    page_size = 10
+from .serializers import PostSerializer
 
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
-    pagination_class = FeedPagination
 
     def get_queryset(self):
         user = self.request.user
-        # posts by users the current user follows OR their own posts
-        following = user.following.all().values_list('pk', flat=True)
-        return Post.objects.filter(author__pk__in=list(following) + [user.pk]).order_by('-created_at')
+        following_users = user.following.all()  # assuming `following` is on CustomUser
+        return Post.objects.filter(author__in=following_users).order_by('-created_at')
 
